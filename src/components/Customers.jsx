07c4cb6caa1b898852f476fdb25049e716cb7e81
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-16 09:55:08 +0200

    customers done

---
+++ src/components/Customers.jsx
@@ -0,0 +1,9 @@
+export const Customers = () => {
+	const companyName = 'ccc';
+	return (
+		<>
+			<h2>Customers</h2>
+			<p>These are the customers of the company named: {companyName}</p>
+		</>
+	);
+};
