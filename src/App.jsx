0fcc28b8d97a1268e50a62d3aee3a7f9eb141679
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-16 09:56:21 +0200

    employees

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Customers } from './components/Customers';
+import { Employees } from './components/Employees';
 
 function App() {
   return (
     <div className="App">
     <h1>Test Question</h1>
     <Customers/>
+    <Employees/>
     </div>
   )
 }
---
+++ src/components/Employees.jsx
@@ -0,0 +1,9 @@
+export const Employees = () => {
+	const companyName = 'eee';
+	return (
+		<>
+			<h2>Employees</h2>
+			<p>These are the employees of the company named: {companyName}</p>
+		</>
+	);
+};
